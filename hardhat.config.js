3d3669c90e26f06342737353e93d60aee3d0bc68
Vedran Vukelic <vedran.vukelic@gmail.com>
2022-09-18 13:08:33 +0200

    Remove frontend from template + add goerli network to hardhat config.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-// The next line is part of the sample project, you don't need it in your
-// project. It imports a Hardhat task definition, that can be used for
-// testing the frontend.
-require("./tasks/faucet");
+const secret = require('./secret.json');
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
   networks: {
-    hardhat: {
-      chainId: 1337 // We set 1337 to make interacting with MetaMask simpler
+    goerli: {
+      url: secret['network_url'],
+      accounts: {
+        mnemonic: secret['wallet_mnemonic']
+      }
     }
   }
 };
