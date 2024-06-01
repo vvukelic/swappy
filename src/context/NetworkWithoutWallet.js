5f60329b991fe3bb78264645b933cb124bd7692b
Vedran Vukelic <vedran.vukelic@gmail.com>
2024-06-01 20:10:30 +0200

    Hardcode ethereum as default network.

--- src/context/NetworkWithoutWallet.js
+++ src/context/NetworkWithoutWallet.js
@@ -1,13 +1,13 @@
 import React, { createContext, useState, useContext } from 'react';
-import { getSupportedNetworks } from '../utils/general';
+import networks from '../data/networks';
 
 const NetworkWithoutWalletContext = createContext({
-    network: getSupportedNetworks()[0],
+    network: networks['ethereum'],
     setNetwork: () => {},
 });
 
 export const NetworkWithoutWalletProvider = ({ children }) => {
-    const [networkWithoutWallet, setNetworkWithoutWallet] = useState(getSupportedNetworks()[0]);
+    const [networkWithoutWallet, setNetworkWithoutWallet] = useState(networks['ethereum']);
 
     return <NetworkWithoutWalletContext.Provider value={{ networkWithoutWallet, setNetworkWithoutWallet }}>{children}</NetworkWithoutWalletContext.Provider>;
 };
