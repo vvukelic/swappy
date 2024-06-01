fbc9e4ddca13cdea71842116c90323bfbafad170
Vedran Vukelic <vedran.vukelic@gmail.com>
2024-06-01 20:06:18 +0200

    Revert some changes.

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,8 +6,7 @@ class MyDocument extends Document {
             <Html>
                 <Head>
                     <link rel='icon' href='../images/favicon/favicon.ico' sizes='48x48' />
-                    <title>Swappy - trade crypto on your own terms!</title>
-                    <meta name='description' content='Swappy is a platform facilitating decentralized, peer-to-peer (P2P) swaps, offering users a trustless environment for over-the-counter (OTC) trading.' />
+                    <title>Swappy</title>
                 </Head>
                 <body>
                     <Main />
